28c9384ed47177f5b91b319a6df0a08bedc59a93
Daniel Paulino <danielpaulino030@gmail.com>
2015-10-04 15:43:48 -0300

    Usando Spinner para indicar carregamento da imagem

--- public/javascripts/components/image_item.js
+++ public/javascripts/components/image_item.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import BaseService from '../services/base';
 import url from 'url';
+import Spinner from './spinner';
 
 const ImageItem = React.createClass({
+  getInitialState: function() {
+    return { loaded: false };
+  },
+
+  loadImage: function() {
+    var img = document.createElement('img');
+    img.addEventListener('load', () => this.setState({ loaded: true }) );
+    img.src = this.getPath(this.props.src);
+  },
+
+  componentWillMount: function() {
+    this.loadImage();
+  },
+
   getPath(src) {
     return url.resolve(BaseService.BASE_URL, src);
   },
@@ -12,7 +27,11 @@ const ImageItem = React.createClass({
     var src = this.props.src,
         title = this.props.title;
 
-    return <img className="thumb" src={ this.getPath(src) } title={ title } />;
+    if (!this.state.loaded) {
+      return(<Spinner />);
+    } else {
+      return <img className="thumb" src={ this.getPath(src) } title={ title } />;
+    }
     /* jshint ignore: end */
   }
 
