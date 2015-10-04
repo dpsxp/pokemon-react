2f87bf381275e0b37e9051b013ad6ce94cff242e
Daniel Paulino <danielpaulino030@gmail.com>
2015-10-04 18:46:47 -0300

    Esperando url absoluta como src

--- public/javascripts/components/image_item.js
+++ public/javascripts/components/image_item.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import BaseService from '../services/base';
 import url from 'url';
 import Spinner from './spinner';
 
@@ -11,26 +10,23 @@ const ImageItem = React.createClass({
   loadImage: function() {
     var img = document.createElement('img');
     img.addEventListener('load', () => this.setState({ loaded: true }) );
-    img.src = this.getPath(this.props.src);
+    img.src = this.props.src;
   },
 
   componentWillMount: function() {
     this.loadImage();
   },
 
-  getPath(src) {
-    return url.resolve(BaseService.BASE_URL, src);
-  },
-
   render() {
     /* jshint ignore: start */
     var src = this.props.src,
+        alt = this.props.alt,
         title = this.props.title;
 
     if (!this.state.loaded) {
       return(<Spinner />);
     } else {
-      return <img className="thumb" src={ this.getPath(src) } title={ title } />;
+      return <img className="thumb" src={ src } alt={ alt } title={ title } />;
     }
     /* jshint ignore: end */
   }
