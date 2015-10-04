51ea5a103de20590325a70bb8a5fbde9172b643d
Daniel Paulino <danielpaulino030@gmail.com>
2015-10-04 15:40:42 -0300

    Novo componente de spinner

---
+++ public/javascripts/components/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = React.createClass({
+  render() {
+    /* jshint ignore: start */
+    return(<div className="mdl-spinner mdl-js-spinner is-active"></div>);
+    /* jshint ignore: end */
+  }
+
+});
+
+export default Spinner;
