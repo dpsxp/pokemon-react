7634d64682374e253a311140b963e9336d4cdd24
Daniel Paulino <danielpaulino030@gmail.com>
2015-10-09 01:29:43 -0300

    Nigthwatch setup

---
+++ features/list_feature.js
@@ -0,0 +1,9 @@
+const TIMEOUT = 3000;
+
+module.exports = {
+  'List of pokemons': function(client) {
+    client
+      .url('http://localhost:3000')
+      .waitForElementVisible('body', TIMEOUT);
+  }
+}
