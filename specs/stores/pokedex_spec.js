392bbc8c11e480a7717a6025ddb2c5c9aeab0cdb
Daniel Paulino <danielpaulino030@gmail.com>
2015-10-09 00:07:06 -0300

    PokedexStore specs

---
+++ specs/stores/pokedex_spec.js
@@ -0,0 +1,71 @@
+import PokedexStore from '../../public/javascripts/stores/pokedex';
+import PokedexService from '../../public/javascripts/services/pokedex';
+import dispatcher from '../../public/javascripts/dispatcher';
+import PokedexActions from '../../public/javascripts/actions/pokedex_actions';
+
+describe('PokedexStore', function() {
+  describe('#getInitialState', function() {
+    it('returns a object with a pokemons array', function() {
+      var state = PokedexStore.getInitialState();
+      expect(state.pokemons).toEqual([]);
+    });
+
+    it('returns a object with a page number', function() {
+      var state = PokedexStore.getInitialState();
+      expect(state.page).toEqual(0);
+    });
+
+    it('returns a object with a finished flag', function() {
+      var state = PokedexStore.getInitialState();
+      expect(state.finished).toEqual(false);
+    });
+  });
+
+  describe('#loadData', function() {
+    it('fetchs the data from PokedexService', function() {
+      var spy = spyOn(PokedexService, 'get');
+      PokedexStore.loadData();
+      expect(spy).toHaveBeenCalledWith(PokedexStore.getState().page, 50);
+    });
+  });
+
+  describe('actions response', function() {
+    describe('pokedex/fetched', function() {
+      it('concats the action pokemon in the current state', function() {
+        var { pokemons } = PokedexStore.getState(),
+            action   = PokedexActions.FETCHED;
+
+        action.pokemons = ['a', 'b', 'c'];
+        action.total = 100;
+
+        dispatcher.dispatch(action);
+
+        expect(PokedexStore.getState().pokemons).toEqual(pokemons.concat(action.pokemons));
+      });
+
+      it('increases the page', function() {
+        var { pokemons, page } = PokedexStore.getState(),
+            action   = PokedexActions.FETCHED;
+
+        action.pokemons = ['a', 'b', 'c'];
+        action.total = 100;
+
+        dispatcher.dispatch(action);
+
+        expect(PokedexStore.getState().page).toEqual(page + 1);
+      });
+
+      it('checks if is in the last page', function() {
+        var { pokemons } = PokedexStore.getState(),
+            action   = PokedexActions.FETCHED;
+
+        action.pokemons = ['a', 'b', 'c'];
+        action.total = 3;
+
+        dispatcher.dispatch(action);
+
+        expect(PokedexStore.getState().finished).toEqual(false);
+      });
+    });
+  });
+});
